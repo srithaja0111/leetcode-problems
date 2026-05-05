74b1f869511dd4fafeef8d8d4555da36bca484f5
srithaja0111 <srithajakandhukoori0111@gmail.com>
2026-05-05 11:12:36 +0530

    Time: 0 ms (100%), Space: 19.2 MB (92.6%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.py
@@ -0,0 +1,15 @@
+class Solution:
+    def rotate(self, matrix: list[list[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        n = len(matrix)
+        
+        # 1. Transpose: Swap elements across the diagonal
+        for i in range(n):
+            for j in range(i + 1, n):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+        
+        # 2. Reverse: Flip each row horizontally
+        for i in range(n):
+            matrix[i].reverse()
